6d2da3ecb58e8b80bc5fdff87130ba999dbc391b
Gopal Jivan <gopal.jivan@gmail.com>
2021-02-27 14:30:52 -0500

    Adds banner based on location selector

--- background.js
+++ background.js
@@ -49,7 +49,36 @@ function updateListener(bannerConfigs) {
 }
 
 function addBanner(details) {
-  chrome.tabs.executeScript(details.tabId, {
-    file: "banner.js",
+  chrome.storage.sync.get("bannerConfigs", (data) => {
+    let querySelector = data.bannerConfigs.filter((config) =>
+      details.url.includes(config.url)
+    )[0].locationSelector;
+    console.log(querySelector);
+    console.log(typeof querySelector);
+    console.log(bannerJs(querySelector));
+    chrome.tabs.executeScript(details.tabId, {
+      // file: "banner.js",
+      code: bannerJs(querySelector),
+    });
   });
+}
+
+function bannerJs(querySelector) {
+  return `
+var d = document.createElement("div");
+d.style.position = "sticky";
+d.style.top = "0px";
+d.style.zIndex = "2147483647";
+d.style.width = "100%";
+d.style.height = "30px";
+d.style.padding = "2px";
+d.style.fontSize = "20px";
+d.style.textAlign = "left";
+d.style.backgroundColor = "#E53935";
+d.style.color = "#333333";
+d.append(document.createTextNode("Production Environment"));
+let bannerParent = document.querySelector("${querySelector}");
+console.log(bannerParent);
+bannerParent.insertBefore(d, bannerParent.firstChild);
+ `;
 }
