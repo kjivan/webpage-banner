95e429d9ab388719d6cc2eee27555b7a51c0252b
Gopal Jivan <gopal.jivan@gmail.com>
2021-02-27 12:53:21 -0500

    Adds Location Selector Text Field to popup

--- popup.js
+++ popup.js
@@ -1,44 +1,55 @@
 let urlContainer = document.getElementById("url-container");
 let addBtn = document.getElementById("add-url");
 
-const urlIndex = 0;
-const removeBtnIndex = 1;
+const urlClass = "url";
+const locationSelectorClass = "locationSelector";
+const defaultSelector = "body";
 
-chrome.storage.sync.get("urls", function (data) {
-  data.urls.forEach(addUrlInput);
+chrome.storage.sync.get("bannerConfigs", function (data) {
+  data.urls.forEach(addInputs);
 });
 
 window.onblur = () => {
-  saveUrls();
+  saveBannerConfigs();
 };
 
 addBtn.onclick = () => {
-  addUrlInput("");
+  addInputs({ url: "", locationSelector: defaultSelector });
 };
 
-function saveUrls() {
+function saveBannerConfigs() {
   let children = urlContainer.children;
-  let urls = [];
+  let bannerConfigs = [];
   for (let i = 0; i < children.length; i++) {
-    urls.push(children[i].children[urlIndex].value);
+    bannerConfigs.push({
+      url: children[i].querySelector(urlClass).value,
+      locationSelector: children[i].querySelector(locationSelector).value,
+    });
   }
-  chrome.storage.sync.set({ urls: urls }, () => {});
+  chrome.storage.sync.set({ bannerConfigs: bannerConfigs }, () => {});
 }
 
-function addUrlInput(url) {
+function addInputs({ url, locationSelector }) {
   let newUrl = document.createElement("input");
-  newUrl.placeholder = "Enter Banner URL";
+  newUrl.placeholder = "Banner URL";
+  newUrl.classList.add(urlClass);
   newUrl.value = url;
+
+  let newLocationSelector = document.createElement("input");
+  newLocationSelector.placeholder = "Location CSS Selector";
+  newLocationSelector.classList.add(locationSelectorClass);
+  newLocationSelector.value = locationSelector;
 
   let removeBtn = document.createElement("button");
   removeBtn.append(document.createTextNode("X"));
   removeBtn.onclick = (event) => {
     event.target.parentElement.remove();
-    saveUrls();
+    saveBannerConfigs();
   };
 
   let div = document.createElement("div");
   div.append(newUrl);
+  div.append(newLocationSelector);
   div.append(removeBtn);
 
   urlContainer.append(div);
